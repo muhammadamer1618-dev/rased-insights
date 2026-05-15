1e8ca05ad5e93fb818e45f3bcbe6a7f09431715c
muhammadamer1618-dev <muhammadamer1618@gmail.com>
2026-05-15 11:26:45 +0300

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,31 @@
+// firebase.js
+
+import { initializeApp }
+from "https://www.gstatic.com/firebasejs/12.2.1/firebase-app.js";
+
+import { getFirestore }
+from "https://www.gstatic.com/firebasejs/12.2.1/firebase-firestore.js";
+
+const firebaseConfig = {
+
+apiKey: "AIzaSyAalS33kADTW9D2RkXXZ27n9TFvd9TAVJM",
+
+authDomain: "englis-results.firebaseapp.com",
+
+projectId: "englis-results",
+
+storageBucket: "englis-results.firebasestorage.app",
+
+messagingSenderId: "467628171773",
+
+appId: "1:467628171773:web:26d848542fdb89aea17994"
+
+};
+
+const app =
+initializeApp(firebaseConfig);
+
+const db =
+getFirestore(app);
+
+export { db };
